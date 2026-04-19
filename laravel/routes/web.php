31a50669ea565e8a0914b84905cf0e9fca6079e7
nohataku <tact_0303@yahoo.co.jp>
2026-04-19 11:13:34 +0900

    ルーティング

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WorkController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Admin\WorkController as AdminWorkController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('welcome');});
+Route::get('/works', [WorkController::class, 'index'])->name('works.index');
+Route::get('/works/{id}', [WorkController::class, 'show'])->name('works.show');
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/works', [AdminWorkController::class, 'index'])->name('works.index');
+    Route::get('/works/create', [AdminWorkController::class, 'create'])->name('works.create');
+    Route::post('/works', [AdminWorkController::class, 'store'])->name('works.store');
+    Route::get('/works/{id}/edit', [AdminWorkController::class, 'edit'])->name('works.edit');
+    Route::put('/works/{id}', [AdminWorkController::class, 'update'])->name('works.update');
+    Route::delete('/works/{id}', [AdminWorkController::class, 'destroy'])->name('works.destroy');
 });
