adfd6f1e48439b561294c2b4c629e135950c5d72
portfolio <g.tact.com@gmail.com>
2026-04-25 07:43:53 +0900

    Update next.config.ts

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 initOpenNextCloudflareForDev();
